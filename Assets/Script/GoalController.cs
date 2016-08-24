ebe0c9ab08dadcf497f2d22d028f3eeb48ab1d50
Plankp T <plankp@outlook.com>
2016-08-23 23:55:17 -0400

    UI updates

--- Assets/Script/GoalController.cs
+++ Assets/Script/GoalController.cs
@@ -7,13 +7,14 @@ public class GoalController : MonoBehaviour
     public GameObject collideObj;
     public string jumpScene;
 
-    void OnCollisionEnter(Collision collision)
+    IEnumerator OnCollisionEnter(Collision collision)
     {
         // Very sketchy way of doing it, make sure the *Goal* is above groud and can only be collided by the sphere
         foreach (var collisionPoint in collision.contacts)
         {
             if (collisionPoint.otherCollider == collideObj.GetComponent<Collider>())
             {
+                yield return GetComponentInParent<Animation>().WhilePlaying("OnCollide");
                 SceneManager.LoadScene(jumpScene);
             }
         }
