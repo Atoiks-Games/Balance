610bfd86410ad59cd6530177070271d692050b64
Plankp T <plankp@outlook.com>
2016-08-24 10:57:43 -0400

    Using state machine instead

--- Assets/Script/GoalController.cs
+++ Assets/Script/GoalController.cs
@@ -5,17 +5,21 @@ using System.Collections;
 public class GoalController : MonoBehaviour
 {
     public GameObject collideObj;
-    public string jumpScene;
+    private Animator anim;
 
-    IEnumerator OnCollisionEnter(Collision collision)
+    void Start()
+    {
+        anim = GetComponentInParent<Animator>();
+    }
+
+    void OnCollisionEnter(Collision collision)
     {
         // Very sketchy way of doing it, make sure the *Goal* is above groud and can only be collided by the sphere
         foreach (var collisionPoint in collision.contacts)
         {
             if (collisionPoint.otherCollider == collideObj.GetComponent<Collider>())
             {
-                yield return GetComponentInParent<Animation>().WhilePlaying("OnCollide");
-                SceneManager.LoadScene(jumpScene);
+                anim.SetTrigger("Collide");
             }
         }
     }
---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadScene : StateMachineBehaviour
+{
+    public string nextScene;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
