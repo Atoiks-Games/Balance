ce60c26918cc75697ea5ac4aed654f5da866d2a1
Plankp T <plankp@outlook.com>
2016-08-26 11:44:57 -0400

    Added auto restart feature on camera

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject bindingObject;
+    public GameObject sphere;
+    public GameObject board;
     public Vector3 offset;
     public RotLocReset[] resetObjects;
+    public float restartHeight = 10; // Note: Only values greater than 0 are being tested
 
     void Start()
     {
@@ -17,19 +19,30 @@ public class CameraController : MonoBehaviour
         if (Input.GetAxis("Quit") < 0)
             Application.Quit();
         if (Input.GetAxis("Quit") > 0)
+            ResetPos();
+
+        if (restartHeight > 0)
         {
-            foreach (var item in resetObjects)
-                if (item != null)
-                    item.ApplyReset();
+            if (Vector3.Distance(sphere.transform.position, board.transform.position) > restartHeight)
+            {
+                ResetPos();
+            }
         }
+    }
+
+    void ResetPos()
+    {
+        foreach (var item in resetObjects)
+            if (item != null)
+                item.ApplyReset();
     }
 
     void FixedUpdate()
     {
-        if (bindingObject != null)
+        if (sphere != null)
         {
-            this.transform.position = bindingObject.transform.position + offset;
-            this.transform.LookAt(bindingObject.transform);
+            this.transform.position = sphere.transform.position + offset;
+            this.transform.LookAt(sphere.transform);
         }
     }
 }
